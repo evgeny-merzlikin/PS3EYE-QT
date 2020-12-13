bb806e5963bd02216a6bc75d5ab2254b1ab571a1
Evgeny Merzlikin <johnny.m@mail.ru>
2020-12-13 17:00:04 +0300

    first ver

--- main.py
+++ main.py
@@ -1,64 +1,101 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+import threading
+import queue
+import time
+import traceback, sys
+
+from ctypes import *
+import cv2
+import sys
+import numpy as np
+
+from working_threads import *
 
 
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        self.setWindowTitle("My super app!!")
+        self.threadpool = QThreadPool()
+        self.threadpool.setMaxThreadCount(1)
+        self.pslib = PS3Lib()
 
-        label = QLabel("Super cool!")
-        label.setAlignment(Qt.AlignCenter)
-        self.setCentralWidget(label)
+        layout = QVBoxLayout()
+        self.l = QLabel("")
+        self.l.setMinimumSize(QSize(640,480))
+        b = QPushButton("Start thread!")
+        c = QPushButton("Stop thread!")
+        b.pressed.connect(self.start_thread)
+        c.pressed.connect(self.stop_thread)
+        layout.addWidget(self.l)
+        layout.addWidget(b)
+        layout.addWidget(c)
+        w= QWidget()
+        w.setLayout(layout)
+        self.setCentralWidget(w)
 
-        toolbar = QToolBar("My main toolbar")
-        toolbar.setIconSize(QSize(16,16))
-        self.addToolBar(toolbar)
-        self.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
+        self.show()
 
-        button_action = QAction(QIcon("webcam.png"), "Start camera", self)
-        button_action.setStatusTip("Start camera thread")
-        button_action.triggered.connect(self.onMyToolbarButtonClick)
-        button_action.setCheckable(True)
-        toolbar.addAction(button_action)
+    @pyqtSlot()
+    def closeEvent(self, event):
+        print("stopping all threads")
+        self.stop_thread()
+        print("closing all cameras")
+        self.pslib.close_all_cams()
+        print("uninit of ps3 lib")
+        self.pslib.uninit_lib()
+        print("app exit now")
 
-        toolbar.addSeparator()
+    @pyqtSlot()
+    def start_thread(self):
+        if not self.pslib.load_lib():
+            QMessageBox.about(self, "Error!", "Cannot load PS3 library")
+            return
+        if not self.pslib.init_lib():
+            QMessageBox.about(self, "Error!", "Cannot init PS3 library")
+            return
+        if self.pslib.count_cams() == 0:
+            QMessageBox.about(self, "Error!", "No PS3 cameras connected")
+            return
+        self.cam = self.pslib.open_cam()
 
-        button_action2 = QAction(QIcon("ui-tab--plus.png"), "Show CV tab", self)
-        button_action2.setStatusTip("Show CV tab")
-        button_action2.triggered.connect(self.onMyToolbarButtonClick)
-        button_action2.setCheckable(True)
-        toolbar.addAction(button_action2)
+        if self.cam is None:
+            QMessageBox.about(self, "Error!", "Cannot open camera")
+            return
+        self.cam.signals.new_frame.connect(self.new_frame_captured)
+        # self.worker = Worker()
+        # self.worker.signals.new_frame.connect(self.new_frame_captured)
+        self.threadpool.start(self.cam)
 
-        toolbar.addWidget(QLabel("Hello"))
-        toolbar.addWidget(QCheckBox())
+    def new_frame_captured(self, bgr):
+        img = QImage(bgr.data, bgr.shape[1], bgr.shape[0], QImage.Format_RGB888).rgbSwapped()
+        self.l.setPixmap(QPixmap.fromImage(img))
 
-        self.setStatusBar(QStatusBar(self))
+    @pyqtSlot()
+    def stop_thread(self):
+        # self.worker.signals.stop.emit()
+        if hasattr(self, 'cam'):
+            print("stopping camera worker thread")
+            self.cam.event_stop.set()
+            self.threadpool.waitForDone()
+            print("camera worker thread stopped")
+            print("closing camera")
+            self.pslib.close_cam(self.cam)
+            print("camera closed")
 
-        menu = self.menuBar()
-
-        file_menu = menu.addMenu("&File")
-        file_menu.addAction(button_action)
-        file_menu.addSeparator()
-        file_menu.addAction(button_action2)
-
-    def onMyToolbarButtonClick(self, s):
-        print("click",s)
-
-    def onWindowTitleChange(self, s):
-        print(s)
-
-    def my_custom_fn(self, a="HELLO!", b=5):
-        print(a, b)
-
-    def contextMenuEvent(self, event):
-        print("Context menu event!")
-        super(MainWindow, self).contextMenuEvent(event)
+    def criticalError(self, message):
+        QMessageBox.about(self, "Error!", message)
+        # self.close()
 
 
-app = QApplication([])
-window = MainWindow()
-window.show()
-app.exec_()
+def main():
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
+
+
+if __name__ == "__main__":
+    main()
